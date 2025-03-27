e3c6d0909421d0024b34be6b5d530cbdb28be488
shankar7979 <shankar7979@gmail.com>
2025-03-27 17:32:05 +0530

    added

---
+++ src/main/java/com/abc/FactorialMain.java
@@ -0,0 +1,16 @@
+package com.abc;
+//
+//import com.capg.Factorial;
+
+import com.abc.dao.Factorial;
+
+public class FactorialMain {
+
+	public static void main(String[] args) {
+	System.out.println("hello factorial");
+    System.out.println(Factorial.factorial(3));
+    System.out.println(Factorial.factorial(4));
+    System.out.println(Factorial.factorial(5));
+    
+	}
+}
